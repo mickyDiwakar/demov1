256a23b20067e4fab749f1a652225b383d2b070c
diwakar kumar <diwisuji@gmail.com>
2021-05-14 15:57:25 +0530

    as

--- src/main/java/package1/class2.java
+++ src/main/java/package1/class2.java
@@ -1,7 +1,13 @@
 package package1;
 
 public class class2 {
+	protected void finalize() {
+		System.out.println("s");
+	}
 	public static void main(String[] args) {
+		class1 c=new class1();
+	int c1=50/0;
 		System.out.println("checked2 in");
+		System.gc();
 	}
 }
